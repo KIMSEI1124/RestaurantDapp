8ef09699cf558371569837d172b12db765704588
KIMSEI <bbomi1973@gmail.com>
2022-06-22 14:40:50 +0900

    smartContract folder, spring boot project Create

---
+++ restaurant/src/test/java/com/restaurantdapp/restaurant/DappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantdapp.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
